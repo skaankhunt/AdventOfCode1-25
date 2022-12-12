7e33c54db907208a73794a788cecbe5898ffa113
skaankhunt <Erik.soderstrom00@gmail.com>
2022-12-12 19:11:33 +0100

    2nd commit

--- 5/main.py
+++ 5/main.py
@@ -1,4 +1,5 @@
 # Real input
+
 containers_dict = {
     'container1': ["Q", "M", "G", "C", "L"],
     'container2': ["R", "D", "L", "C", "T", "F", "H", "G"],
@@ -57,83 +58,25 @@ for i in range(len(containers_dict)):
 
 # Part 2
 for i in range(len(move_from)):
-    temp = move_amount[i]
+    lenght_from = len(containers_dict['container' + str(move_from[i])]) - 1
+    b = lenght_from
+    a = b + 1 - move_amount[i]
+
     print("Line : " + str(i+1))
-    #print("Amount to move : " + str(move_amount[i]))
-    #print("Available amount : " + str(len(containers_dict['container' + str(move_from[i])])))
-    available_amount = len(containers_dict['container' + str(move_from[i])])
-    amount_to_move = move_amount[i]
-    if amount_to_move > available_amount:
-        print("Not enough characters to move")
-
-    for j in range(move_amount[i]):
-        if temp >= 3:
-            # Append the 3 last characters to new container
-            containers_dict['container' + str(move_to[i])].append(containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-3])
-            containers_dict['container' + str(move_to[i])].append(containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-2])
-            containers_dict['container' + str(move_to[i])].append(containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-1])
-            
-            #print("Moving : " + containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-3] + " From container" + str(move_from[i]) + " to " + "container" + str(int(move_to[i])))
-            #print("Moving : " + containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-2] + " From container" + str(move_from[i]) + " to " + "container" + str(int(move_to[i])))
-            #print("Moving : " + containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-1] + " From container" + str(move_from[i]) + " to " + "container" + str(int(move_to[i])))
-
-            # Delete the appended characters from old container
+    print("Move amount = " + str(move_amount[i]))
+    print("Lenght of move_from = " + str(len(containers_dict['container' + str(move_from[i])])-1))
+    print("a = " + str(a))
+    print("b = " + str(b))
+    
+    if move_amount[i] == 1:
+        containers_dict['container' + str(move_to[i])].append(containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-1])
+        print(containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-1])
+        containers_dict['container' + str(move_from[i])].pop()
+    else:
+        containers_dict['container' + str(move_to[i])].extend(containers_dict['container' + str(move_from[i])][a:b+1])
+        print(containers_dict['container' + str(move_from[i])][a:b+1])
+        for x in range(move_amount[i]):
             containers_dict['container' + str(move_from[i])].pop()
-            containers_dict['container' + str(move_from[i])].pop()
-            containers_dict['container' + str(move_from[i])].pop()
-            
-            #print("Container" + str(move_from[i]) + " now looks like this : " + str(containers_dict['container' + str(move_from[i])]))
-            #print("Container" + str(move_to[i]) + " now looks like this : " + str(containers_dict['container' + str(move_to[i])]))
-
-            temp -= 3
-            #print("Remaining amount to move : " + str(temp))
-            if temp == 0:
-                print("Done moving.. Next line!")
-                break
-
-        elif temp == 2:
-            # Append the 2 last characters to new container
-            containers_dict['container' + str(move_to[i])].append(containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-2])
-            containers_dict['container' + str(move_to[i])].append(containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-1])
-            
-            #print("Moving : " + containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-2] + " From container" + str(move_from[i]) + " to " + "container" + str(int(move_to[i])))
-            #print("Moving : " + containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-1] + " From container" + str(move_from[i]) + " to " + "container" + str(int(move_to[i])))
-            
-            # Delete the appended characters from old container
-            containers_dict['container' + str(move_from[i])].pop()
-            containers_dict['container' + str(move_from[i])].pop()
-
-            #print("Container" + str(move_from[i]) + " now looks like this : " + str(containers_dict['container' + str(move_from[i])]))
-            #print("Container" + str(move_to[i]) + " now looks like this : " + str(containers_dict['container' + str(move_to[i])]))
-
-            temp -= 2
-            
-            #print("Remaining amount to move : " + str(temp))
-            
-            if temp == 0:
-                print("Done moving.. Next line!")
-                break
-
-        elif temp == 1:
-            # Append the last character to new container
-            containers_dict['container' + str(move_to[i])].append(containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-1])
-            
-            #print("Moving : " + containers_dict['container' + str(move_from[i])][len(containers_dict['container' + str(move_from[i])])-1] + " From container" + str(move_from[i]) + " to " + "container" + str(int(move_to[i])))
-            
-            # Delete the appended characters from old container
-            containers_dict['container' + str(move_from[i])].pop()
-            
-            #print("Container" + str(move_from[i]) + " now looks like this : " + str(containers_dict['container' + str(move_from[i])]))
-            #print("Container" + str(move_to[i]) + " now looks like this : " + str(containers_dict['container' + str(move_to[i])]))
-            
-            temp -= 1
-
-            #print("Remaining amount to move : " + str(temp))
-            print("Done moving.. Next line!")
-            break
-        else:
-            print("Done moving.. Next line!")
-            break
 
 print("Finished")
 for i in range(len(containers_dict)):
